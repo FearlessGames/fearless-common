9779f65ccb2db876c79737109d072f85cda32525
hiflyer <per.malmen@gmail.com>
2015-04-10 16:51:29 +0200

    Added tests for remove random element

--- core/src/test/java/se/fearless/common/collections/Collections3Test.java
+++ core/src/test/java/se/fearless/common/collections/Collections3Test.java
@@ -35,6 +35,39 @@ public class Collections3Test {
 	}
 
 	@Test
+	public void removeRandomElement() throws Exception {
+		List<String> strings = Lists.newArrayList("a", "b", "c", "d", "e");
+
+		Random random = new Random();
+
+		List<String> removedStrings = Lists.newArrayList();
+		for (int i = 0; i < 5; i++) {
+			String removedString = Collections3.removeRandomElement(strings, random);
+			removedStrings.add(removedString);
+		}
+		assertEquals(5, removedStrings.size());
+		assertTrue(removedStrings.contains("a"));
+		assertTrue(removedStrings.contains("b"));
+		assertTrue(removedStrings.contains("c"));
+		assertTrue(removedStrings.contains("d"));
+		assertTrue(removedStrings.contains("e"));
+	}
+
+
+	@Test
+	public void removeRandomElementReturnsNullAfterAllElementsAreDepleted() throws Exception {
+		List<String> strings = Lists.newArrayList("a", "b");
+
+		Random random = new Random();
+
+		Collections3.removeRandomElement(strings, random);
+		Collections3.removeRandomElement(strings, random);
+
+		String removedElement = Collections3.removeRandomElement(strings, random);
+		assertNull(removedElement);
+	}
+
+	@Test
 	public void sumIntegerList() throws Exception {
 		List<Integer> ints = Lists.newArrayList(2, 1, 3, 4);
 		long sum = Collections3.sum(ints, new Collections3.Function<Integer, Integer>() {
