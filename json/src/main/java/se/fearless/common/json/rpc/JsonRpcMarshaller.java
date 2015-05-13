25869fc5905d7f90f2e43908bfd4903b7214a311
hiflyer <per.malmen@gmail.com>
2015-05-13 16:02:26 +0200

    Added basic json rpc data structures and marshalling

---
+++ json/src/main/java/se/fearless/common/json/rpc/JsonRpcMarshaller.java
@@ -0,0 +1,41 @@
+package se.fearless.common.json.rpc;
+
+import se.fearless.common.json.JsonSerializer;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class JsonRpcMarshaller {
+
+	private final JsonSerializer jsonSerializer;
+
+	public JsonRpcMarshaller(JsonSerializer jsonSerializer) {
+		this.jsonSerializer = jsonSerializer;
+	}
+
+
+	public String formatResponse(List<JsonRpcResponse> jsonRpcResponses) {
+		StringBuilder result = new StringBuilder("[");
+
+		boolean notFirstRequest = false;
+		for (JsonRpcResponse jsonRpcResponse : jsonRpcResponses) {
+			String individualResult = jsonSerializer.toJson(jsonRpcResponse);
+			if (individualResult != null && individualResult.length() > 0) {
+				if (notFirstRequest) {
+					result.append(", ");
+				}
+				result.append(individualResult);
+				notFirstRequest = true;
+			}
+		}
+		result.append("]");
+		return result.toString();
+
+
+	}
+
+	public List<JsonRpcRequest> parseContent(String content) {
+		JsonRpcRequest[] jsonRpcRequests = jsonSerializer.fromJson(content, JsonRpcRequest[].class);
+		return Arrays.asList(jsonRpcRequests);
+	}
+}
---
+++ json/src/main/java/se/fearless/common/json/rpc/JsonRpcResponse.java
@@ -0,0 +1,7 @@
+package se.fearless.common.json.rpc;
+
+public class JsonRpcResponse {
+	public String jsonrpc;
+	public Long id;
+	public Object result;
+}
