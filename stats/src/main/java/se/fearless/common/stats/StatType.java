164f185937a909b9cee14fe7640815463691dbe1
hiflyer <per.malmen@gmail.com>
2016-03-24 16:37:24 +0100

    Fixed the comparable type issue with StatType

--- stats/src/main/java/se/fearless/common/stats/StatType.java
+++ stats/src/main/java/se/fearless/common/stats/StatType.java
@@ -1,5 +1,5 @@
 package se.fearless.common.stats;
 
-public interface StatType extends Comparable<StatType> {
+public interface StatType<T extends StatType> extends Comparable<T> {
 	String getName();
 }
